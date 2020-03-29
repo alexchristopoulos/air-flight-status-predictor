b133f66f49dfdf4324b387ce8020e75352178124
Alex SW <alex1951132@gmail.com>
2020-03-29 14:08:02 +0300

    random forest code optimized

--- src/main/scala/predictor/classification/RandomForestClassification.scala
+++ src/main/scala/predictor/classification/RandomForestClassification.scala
@@ -4,9 +4,8 @@ import gr.upatras.ceid.ddcdm.predictor.datasets.AirFlightsKaggleDataset
 import gr.upatras.ceid.ddcdm.predictor.datasets.AirlinesDataset
 import gr.upatras.ceid.ddcdm.predictor.datasets.AirportsKaggleDataset
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, RFormula, StringIndexer, VectorIndexer}
+import org.apache.spark.ml.classification.{ RandomForestClassifier }
+import org.apache.spark.ml.feature.{ RFormula, StringIndexer, VectorAssembler }
 import gr.upatras.ceid.ddcdm.predictor.spark.Spark
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
@@ -29,41 +28,36 @@ object RandomForestClassification {
         "INNER JOIN airports AS a2 ON f.DESTINATION_AIRPORT=a2.iata")
         .createOrReplaceTempView("FLIGHTS_DATA")
 
-    /**
-      * 362035 in class 0 >> no delay
-      * 95951 in class 1 >> delay
-      * 11982 in class 2 >> cancelled
-      */
-
     val delays = sparkSession.sql("SELECT * FROM FLIGHTS_DATA WHERE CANCELLED=1")
     val cancelations = sparkSession.sql("SELECT * FROM FLIGHTS_DATA WHERE CANCELLED=2")
-    val noDelays = sparkSession.sql("SELECT * FROM FLIGHTS_DATA WHERE CANCELLED=0 LIMIT " + ((delays.count() + cancelations.count())/3).toString())
+    val noDelays = sparkSession.sql("SELECT * FROM FLIGHTS_DATA WHERE CANCELLED=0 ")
 
-    val splitCombined = delays
+    val stringIndexer = new StringIndexer()
+      .setInputCol("CANCELLED")
+      .setOutputCol("label")
+
+    val featuresNames = Array("DAY", "DAY_OF_WEEK", "AIRLINE_ID", "ORIGIN", "DESTINATION", "ARRIVAL_DELAY", "DISTANCE")
+
+    val vectorAssembler = new VectorAssembler()
+      .setInputCols(featuresNames)
+      .setOutputCol("features")
+
+    var Array(pipelineTrainData, pipelineTestData) = delays
       .union(cancelations)
       .union(noDelays)
-      .randomSplit(Array(0.6, 0.4))
+      .randomSplit(Array(0.65, 0.35), 11L)
 
-    val formula = new RFormula()
-      .setFormula("CANCELLED ~ DAY + DAY_OF_WEEK + AIRLINE_ID + ORIGIN + DESTINATION + ARRIVAL_DELAY + DISTANCE")
-      .setFeaturesCol("features")
-      .setLabelCol("label")
+    pipelineTrainData.cache()
 
-    val trainrf = formula
-      .fit(splitCombined(0))
-      .transform(splitCombined(0))
-      .cache()
+    val randomForestClassifier = new RandomForestClassifier()
+        .setNumTrees(10)
 
-    val testrf = formula
-      .fit(splitCombined(1))
-      .transform(splitCombined(1))
+    val stages = Array(vectorAssembler, stringIndexer, randomForestClassifier)
+    val pipeline = new Pipeline().setStages(stages)
 
-    val model = new RandomForestClassifier()
-      .setNumTrees(10)
-      .fit(trainrf)
+    val model = pipeline.fit(pipelineTrainData)
 
-    val predictions = model
-      .transform(testrf)
+    val predictions = model.transform(pipelineTestData)
 
     val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("label")
@@ -72,6 +66,8 @@ object RandomForestClassification {
 
     val accuracy = evaluator
       .evaluate(predictions)
+
+    model.write.overwrite().save("/home/admin/randomForestModel")
 
     val rddEval: org.apache.spark.rdd.RDD[(Double, Double)] = predictions.rdd.map(row => ( row(12).toString().toDouble, row(9).toString().toDouble ))
 
@@ -82,7 +78,12 @@ object RandomForestClassification {
       println(label.toString() + "  " + metrics.precision(label).toString() + "  " + metrics.recall(label).toString() + "   " + metrics.fMeasure(label).toString())
     })
 
+    println("\n\nWEIGHTED_PRECISION   WEIGHTED_RECALL   WEIGHTED_F1")
+    println(metrics.weightedPrecision + "   " + metrics.weightedRecall + "   " + metrics.weightedFMeasure + "\n")
+
     println(s"Test set accuracy = $accuracy")
+
+
 
   }
 
--- src/main/scala/predictor/datasets/AirlinesDataset.scala
+++ src/main/scala/predictor/datasets/AirlinesDataset.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import gr.upatras.ceid.ddcdm.predictor.config.config
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import gr.upatras.ceid.ddcdm.predictor.util.FuncOperators
 import gr.upatras.ceid.ddcdm.predictor.spark.Spark
 
@@ -15,7 +15,7 @@ object AirlinesDataset {
   private var datasetDf: DataFrame = _
 
   private val struct = StructType(
-    StructField("id", StringType, false) ::
+    StructField("id", IntegerType, false) ::
     StructField("iata", StringType, false) ::
       StructField("name", StringType, false) :: Nil)
 
@@ -25,7 +25,11 @@ object AirlinesDataset {
       .getSparkContext()
       .textFile(config.sparkDatasetDir + config.sparkDatasetPredictionAirlines)
       .mapPartitionsWithIndex(FuncOperators.removeFirstLine)
-      .map(FuncOperators.csvStringRowToRow)
+      .map(line => FuncOperators.csvStringRowToRowType(line, Map(
+        0 -> "Int",
+        1 -> "String",
+        2 -> "String"
+      )))
 
     this.datasetDf = Spark
       .getSparkSession()
