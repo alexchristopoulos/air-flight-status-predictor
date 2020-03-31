a940e6556b9675ceaab430f45b7243072632fcb2
alexchristopoulos <alex1951132@gmail.com>
2020-03-31 19:53:44 +0300

    train dataset types change

--- src/main/scala/predictor/datasets/TrainDataset.scala
+++ src/main/scala/predictor/datasets/TrainDataset.scala
@@ -19,27 +19,28 @@ object TrainDataset {
     "MONTH" -> Tuple2(1, "Int"),
     "DAY_OF_MONTH" -> Tuple2(2, "Int"),
     "DAY_OF_WEEK" -> Tuple2(3, "Int"),
-    "OP_CARRIER" -> Tuple2(4, "Int"),
-    "TAIL_NUM" -> Tuple2(5, "Int"),
-    "OP_CARRIER_FL_NUM" -> Tuple2(6, "Int"),
-    "ORIGIN" -> Tuple2(7, "Int"),
-    "DEST" -> Tuple2(8, "Int"),
-    "DEP_DELAY_NEW" -> Tuple2(9, "Int"),
-    "TAXI_OUT" -> Tuple2(10, "Int"),
+    "OP_CARRIER" -> Tuple2(4, "String"),//airline iata code
+    "TAIL_NUM" -> Tuple2(5, "String"),
+    "OP_CARRIER_FL_NUM" -> Tuple2(6, "Int"),//flight number for op carrier
+    "ORIGIN" -> Tuple2(7, "String"),//origin airpotr iata code
+    "DEST" -> Tuple2(8, "String"),//destination airpotr iata code
+    "DEP_DELAY_NEW" -> Tuple2(9, "Double"),//departure delay in minutes
+    "TAXI_OUT" -> Tuple2(10, "Double"),
     "WHEELS_OFF" -> Tuple2(11, "Int"),
     "WHEELS_ON" -> Tuple2(12, "Int"),
-    "TAXI_IN" -> Tuple2(13, "Int"),
-    "ARR_DELAY_NEW" -> Tuple2(14, "Int"),
-    "CANCELLED" -> Tuple2(15, "Int"),
-    "CANCELLATION_CODE" -> Tuple2(16, "Int"),
-    "DIVERTED" -> Tuple2(17, "Int"),
-    "AIR_TIME" -> Tuple2(18, "Int"),
-    "FLIGHTS" -> Tuple2(19, "Int"),
-    "DISTANCE" -> Tuple2(20, "Int")
+    "TAXI_IN" -> Tuple2(13, "Double"),
+    "ARR_DELAY_NEW" -> Tuple2(14, "Double"),
+    "CANCELLED" -> Tuple2(15, "Double"),
+    "CANCELLATION_CODE" -> Tuple2(16, "String"),
+    "DIVERTED" -> Tuple2(17, "Double"),
+    "AIR_TIME" -> Tuple2(18, "Double"),
+    "FLIGHTS" -> Tuple2(19, "Double"),
+    "DISTANCE" -> Tuple2(20, "Double")
   )
 
   def load(): Unit = {
 
+    this.initStruct();
   }
 
   def getDataFrame(): DataFrame = {
